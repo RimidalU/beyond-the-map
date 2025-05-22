bcd19c5d87d1e4733738bd52f303eb93da30c206
uladzimir <stankevichuuladimiru@gmail.com>
2025-05-22 12:32:05 +0300

    feat: add config module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule)
+
+    const config = await app.get(ConfigService)
+
+    console.log(config.get<number>('PORT'))
+
+    const port = config.get<number>('PORT') ?? 3000
+
+    await app.listen(port, () =>
+        console.log(`Server is listening on port ${port}`),
+    )
 }
-bootstrap();
+
+bootstrap()
