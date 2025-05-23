7506f7e06a13923795bbf4ebbe2cbe34cbad3694
uladzimir <stankevichuuladimiru@gmail.com>
2025-05-23 15:44:07 +0300

    feat: add loger to  app

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import { NestFactory } from '@nestjs/core'
 import { ConfigService } from '@nestjs/config'
+import { Logger } from '@nestjs/common'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+    const logger = new Logger('MainApplication')
 
     const config = app.get(ConfigService)
 
     const port = config.get<number>('PORT') ?? 3000
 
-    await app.listen(port)
+    await app.listen(port, () =>
+        logger.log(`Server is listening on port ${port}`),
+    )
 }
 
 bootstrap()
