67b17743885e18f157d381c87249a01fd257afc4
uladzimir <stankevichuuladimiru@gmail.com>
2025-05-24 20:16:53 +0300

    feat: implement remove-swagger decorator

---
+++ src/users/decorators/remove-swagger.decorator.ts
@@ -0,0 +1,30 @@
+import { applyDecorators } from '@nestjs/common'
+import {
+    ApiInternalServerErrorResponse,
+    ApiNotFoundResponse,
+    ApiOperation,
+    ApiResponse,
+} from '@nestjs/swagger'
+
+import { SuccessResponseDto } from '../dto/success-response.dto'
+import { InternalServerErrorResponseDto } from '../dto/internal--server-error-response.dto'
+import { UserNotFoundDTO } from '../dto/user-note-found.response.dto'
+
+export function RemoveSwaggerDecorator() {
+    return applyDecorators(
+        ApiOperation({ summary: 'Delete User' }),
+        ApiInternalServerErrorResponse({
+            description: 'Internal server error',
+            type: InternalServerErrorResponseDto,
+        }),
+        ApiNotFoundResponse({
+            description: 'Not Found',
+            type: UserNotFoundDTO,
+        }),
+        ApiResponse({
+            status: 204,
+            description: 'User removed',
+            type: SuccessResponseDto,
+        }),
+    )
+}
