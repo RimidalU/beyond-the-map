69341547cb41165bcb21554503be72702fca5691
uladzimir <stankevichuuladimiru@gmail.com>
2025-05-24 20:14:43 +0300

    feat: add exeptions

--- src/users/exceptions/database-error.exception.ts
+++
@@ -1,7 +0,0 @@
-import { InternalServerErrorException } from '@nestjs/common'
-
-export class DatabaseError extends InternalServerErrorException {
-    constructor() {
-        super(`Database error`)
-    }
-}
