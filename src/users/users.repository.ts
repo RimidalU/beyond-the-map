e901ec907d91328673edc5e17a5b7ba2f2b9095d
uladzimir <stankevichuuladimiru@gmail.com>
2025-05-24 20:37:41 +0300

    feat: update users repoository

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,14 +1,15 @@
 import { Injectable, Logger } from '@nestjs/common'
-import { Repository } from 'typeorm'
+import { DeleteResult, Repository } from 'typeorm'
 import { InjectRepository } from '@nestjs/typeorm'
-import { UNIQUE_VIOLATION_CODE } from '@src/constants/typeorm.constnts'
 import { TypeOrmError } from '@src/types/typeorm.types'
+import { UNIQUE_VIOLATION_CODE } from '@src/constants/typeorm.constants'
 
 import { UsersEntity } from './entities/users.entity'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
-import { DatabaseError } from './exceptions/database-error.exception'
+import { InternalServerError } from './exceptions/internal-server-error.exception'
 import { UserCannotBeCreated } from './exceptions/user-cannot-be-created.exception'
+import { UserResponseDto } from './dto/user.response.dto'
 
 @Injectable()
 export class UsersRepository {
@@ -28,28 +29,53 @@ export class UsersRepository {
                 throw new UserCannotBeCreated(UsersEntityData.email)
             } else {
                 this.logger.error('Error on creating user', error)
-                throw new DatabaseError()
+                throw new InternalServerError()
             }
         }
     }
 
-    async findAll(): Promise<UsersEntity[]> {
-        return this.repo.find()
+    async findAll(): Promise<UserResponseDto[]> {
+        try {
+            return this.repo.find({
+                select: {
+                    id: true,
+                    username: true,
+                },
+            })
+        } catch (error) {
+            this.logger.error('Error on finding all users', error)
+            throw new InternalServerError()
+        }
     }
 
     async findById(id: number): Promise<UsersEntity | null> {
-        return this.repo.findOneBy({ id })
+        try {
+            return this.repo.findOneBy({ id })
+        } catch (error) {
+            this.logger.error('Error on find user by id', error)
+            throw new InternalServerError()
+        }
     }
 
     async updateById(
         id: number,
         updateData: UpdateUserDto,
     ): Promise<UsersEntity | null> {
-        await this.repo.update(id, updateData)
-        return this.findById(id) ?? null
+        try {
+            await this.repo.update(id, updateData)
+            return this.findById(id) ?? null
+        } catch (error) {
+            this.logger.error('Error on update user', error)
+            throw new InternalServerError()
+        }
     }
 
-    async deleteById(id: number): Promise<void> {
-        await this.repo.delete(id)
+    async deleteById(id: number): Promise<DeleteResult> {
+        try {
+            return await this.repo.delete(id)
+        } catch (error) {
+            this.logger.error('Error on delete user', error)
+            throw new InternalServerError()
+        }
     }
 }
