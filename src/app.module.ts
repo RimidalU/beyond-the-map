2a5995b8b94c9f6b300800ff459661f7e75168e2
uladzimir <stankevichuuladimiru@gmail.com>
2025-05-26 19:31:10 +0300

    feat: add auth medleware to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
@@ -7,6 +8,7 @@ import { DatabaseModule } from './database/database.module'
 import { UsersModule } from './users/users.module'
 import { ArticlesModule } from './articles/articles.module'
 import { AuthModule } from './auth/auth.module'
+import { AuthMiddleware } from './auth/middlewares/auth.middleware'
 
 @Module({
     imports: [
@@ -18,8 +20,16 @@ import { AuthModule } from './auth/auth.module'
         UsersModule,
         ArticlesModule,
         AuthModule,
+        JwtModule,
     ],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(AuthMiddleware).forRoutes({
+            path: '*',
+            method: RequestMethod.ALL,
+        })
+    }
+}
