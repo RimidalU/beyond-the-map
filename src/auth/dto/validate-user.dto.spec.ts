3f5d1033889c675fdf9800badfbdb801b61674b0
uladzimir <stankevichuuladimiru@gmail.com>
2025-05-27 13:09:10 +0300

    tests: implement create-user.dto tests

---
+++ src/auth/dto/validate-user.dto.spec.ts
@@ -0,0 +1,65 @@
+import { CreateUserDto } from '@src/users/dto/create-user.dto'
+import { plainToInstance } from 'class-transformer'
+import { validate } from 'class-validator'
+
+describe('create-user.dto', () => {
+    let dto: { email?: unknown; password?: unknown }
+    const numberValue = 12
+    const stringValue = 'string-value'
+    const emailValue = 'user@email.com'
+
+    beforeAll(() => {
+        dto = {
+            email: '',
+            password: '',
+        }
+    })
+
+    it('password field is empty', async () => {
+        dto.password = ''
+        const ofImportDTO = plainToInstance(CreateUserDto, dto)
+        const errors = await validate(ofImportDTO)
+        expect(
+            errors.map((err) => err.property).includes('password'),
+        ).toBeTruthy()
+    })
+
+    it('password field is not a string', async () => {
+        dto.password = numberValue
+        const ofImportDTO = plainToInstance(CreateUserDto, dto)
+        const errors = await validate(ofImportDTO)
+        expect(
+            errors.map((err) => err.property).includes('password'),
+        ).toBeTruthy()
+    })
+
+    it('password field is correct', async () => {
+        dto.password = stringValue
+        const ofImportDTO = plainToInstance(CreateUserDto, dto)
+        const errors = await validate(ofImportDTO)
+        expect(
+            errors.map((err) => err.property).includes('password'),
+        ).toBeFalsy()
+    })
+
+    it('email field is empty', async () => {
+        dto.email = ''
+        const ofImportDTO = plainToInstance(CreateUserDto, dto)
+        const errors = await validate(ofImportDTO)
+        expect(errors.map((err) => err.property).includes('email')).toBeTruthy()
+    })
+
+    it('email field is not a email', async () => {
+        dto.email = stringValue
+        const ofImportDTO = plainToInstance(CreateUserDto, dto)
+        const errors = await validate(ofImportDTO)
+        expect(errors.map((err) => err.property).includes('email')).toBeTruthy()
+    })
+
+    it('email field is correct', async () => {
+        dto.email = emailValue
+        const ofImportDTO = plainToInstance(CreateUserDto, dto)
+        const errors = await validate(ofImportDTO)
+        expect(errors.map((err) => err.property).includes('email')).toBeFalsy()
+    })
+})
