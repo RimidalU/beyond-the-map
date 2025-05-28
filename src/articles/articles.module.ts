3e49c8466b69179dfad87b6fbcecd8ef74cdcc7a
uladzimir <stankevichuuladimiru@gmail.com>
2025-05-28 09:39:12 +0300

    feat: add cache to app

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -4,7 +4,7 @@ import { UsersEntity } from '@src/users/entities/users.entity'
 import { UsersRepository } from '@src/users/users.repository'
 import { CacheModule } from '@nestjs/cache-manager'
 import { ConfigModule, ConfigService } from '@nestjs/config'
-import { getCacheModuleConfig } from '@src/config/get-cache-module.config'
+import { getKeyvModuleConfig } from '@src/config/get-keyv-module.config'
 
 import { ArticlesService } from './articles.service'
 import { ArticleEntity } from './entities/articles.entity'
@@ -17,7 +17,7 @@ import { ArticlesController } from './articles.controller'
         CacheModule.registerAsync({
             imports: [ConfigModule],
             inject: [ConfigService],
-            useFactory: getCacheModuleConfig,
+            useFactory: getKeyvModuleConfig,
         }),
     ],
     controllers: [ArticlesController],
